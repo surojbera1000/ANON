621283ae7c87354402e57d2862dfbdaed4b3b6b8
surojbera1000 <sandipberaind@gmail.com>
2026-04-06 12:46:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
+    "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", None
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Bots_Networks")
